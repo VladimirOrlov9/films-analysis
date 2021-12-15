75352eef03673930d229a756497b99ed94fd3c8d
Daniil Afanasyev <afanasyev.dp@gmail.com>
2021-12-15 16:30:05 +0300

    Refactor Docker
    Now url of connector is env variable for shell container.
    Add Type of task for spring container as a env varivable

--- Docker/init-connector/initialize-container.sh
+++ Docker/init-connector/initialize-container.sh
@@ -6,8 +6,8 @@ function test_systems_available {
       printf '.'
       sleep 2
       let COUNTER+=1
-      if [[ $COUNTER -gt 30 ]]; then
-        MSG="\nWARNING: Could not reach configured kafka system on http://$1:$2 \nNote: This script requires curl.\n"
+      if [[ $COUNTER -gt 60 ]]; then
+        MSG="\nWARNING: Could not reach configured system on http://$1:$2 \nNote: This script requires curl.\n"
 
           if [[ "$OSTYPE" == "darwin"* ]]; then
             MSG+="\nIf using OSX please try reconfiguring Docker and increasing RAM and CPU. Then restart and try again.\n\n"
@@ -21,11 +21,11 @@ function test_systems_available {
 
 sleep 5
 echo -ne "\n\nWaiting for the systems to be ready.."
-test_systems_available connect 8083
+test_systems_available $CONNECTOR_HOST $CONNECTOR_PORT
 
 echo -e "\nKafka Connectors adding..."
-curl -X POST -H "Content-Type: application/json" --data @sink-connector.json http://connect:8083/connectors -w "\n"
+curl -X POST -H "Content-Type: application/json" --data @sink-connector.json http://$CONNECTOR_HOST:$CONNECTOR_PORT/connectors -w "\n"
 
 sleep 5
 echo -e "\nKafka Connectors:"
-curl -X GET "http://connect:8083/connectors" -w "\n"
+curl -X GET "http://$CONNECTOR_HOST:$CONNECTOR_PORT/connectors" -w "\n"
--- Docker/init-spring/start-spring.sh
+++ Docker/init-spring/start-spring.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
-java -version
+function start_Collection {
+  java -version
+  java -jar collection.jar
+}
 
-java -jar collection.jar
+function start_Spark {
+  echo "TODO: start Spark app..."
+}
 
-sleep 15
+echo "Type of running is $TASK_RUNNING"
+
+case $TASK_RUNNING in
+COLLECTION)
+  echo "----------Step 1/1: Start Collection Application----------"
+  start_Collection;;
+SPARK)
+  echo "----------Step 1/1: Start Spark Application----------"
+  start_Spark;;
+ALL)
+  echo "----------Step 1/2: Start Collection Application----------"
+  start_Collection
+  sleep 15
+  echo "----------Step 2/2: Start Spark Application----------"
+  start_Spark;;
+*) echo "Error: $TASK_RUNNING is not an option"
+  echo "(Use 'COLLECTION' for Collection, 'SPARK' for Spark Application, 'ALL' for All Steps)" ;;
+esac
 
